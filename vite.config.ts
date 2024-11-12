df624ff9f9adeb436a5343f3a24cfb1c9bccd09e
azertypow <nico@villa1203.ch>
2024-11-12 19:23:52 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'association_relier.website',
   plugins: [vue()],
   resolve: {
     alias: {

b6da8a9ddf71cda4e0c20548a73b3f8134897a2a
azertypow <nico@villa1203.ch>
2024-11-12 19:05:36 +0100

    feat: update content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,15 +9,18 @@
 
         <div class="s-page_content">
 
-          <p>L’association RELIER a pour mission est de créer du lien, de renforcer la solidarité et de favoriser l'inclusion sociale des personnes issus de la migration présente sur le canton de Genève.</p>
-          <p>À travers nos actions, nous nous efforçons de tisser un réseau de solidarité où chacun peut trouver un soutien adapté à ses besoins. Qu'il s'agisse de soutien psychologique, d'accompagnement social, d'accès à des ressources essentielles ou de la mise en place d'activités collectives pour briser l'isolement, notre objectif est de renforcer les liens humains, promouvoir le vivre-ensemble et bâtir un avenir plus solidaire et inclusif.</p>
-          <p>L’association RELIER est née au printemps 2024 pour proposer un accompagnement novateur et agile pour favoriser l’insertion et s’inscrire dans un monde robuste. Les modèles d’accompagnement s’inspirent de « l’Aller Vers » et partage les valeurs de l’éducation populaire.</p>
+
+
+
+          <p>L’association RELIER a pour mission de créer du lien, de renforcer la solidarité et de favoriser l'inclusion sociale des personnes issues de la migration présentes sur le canton de Genève.</p>
+          <p>À travers nos actions, nous tissons et développons un réseau où chacun peut trouver un soutien adapté à ses besoins. Qu'il s'agisse de soutien psychologique, d'accompagnement social, d'accès à des ressources de bases ou de la mise en place d'activités collectives pour briser l'isolement, notre objectif est de renforcer les liens humains, promouvoir le vivre-ensemble et bâtir un avenir plus solidaire et inclusif.</p>
+          <p>L’association RELIER est née au printemps 2024 pour proposer un accompagnement novateur et agile pour favoriser l’insertion et s’inscrire dans un monde «&nbsp;robuste&nbsp;». Les modèles d’accompagnement s’inspirent de «&nbsp;l’Aller Vers&nbsp;» et partagent les valeurs de l’éducation populaire. À&nbsp;savoir…</p>
 
           <div>
             <blockquote>
-              <p>"L'éducation populaire n'est pas seulement une pédagogie, mais une démarche militante qui va à la rencontre des gens pour co-construire du savoir et de la citoyenneté."</p>
+              <p>«&nbsp;L’éducation populaire n'est pas seulement une pédagogie, mais une démarche militante qui va à la rencontre des gens pour co-construire du savoir et de la citoyenneté.&nbsp;»</p>
             </blockquote>
-            <p>— Pépin Maryvonne. <cite>"L’éducation populaire : Histoire et postérité d’une utopie d’émancipation."</cite></p>
+            <p>— Pépin, M. (2004).<cite>L'éducation populaire : Histoire et postérité d'une utopie d'émancipation. L'Harmattan.</cite></p>
           </div>
 
 
@@ -26,47 +29,47 @@
       <section class="app-section-box__section" id="inspiration">
         <h1>Prestations</h1>
         <div class="s-page_content">
-          <h2>Pour répondre à ses missions, l’association RELIER développe des prestations a destination du public cible et de la société civile.</h2>
+          <h2>Pour répondre à ses missions, l’association RELIER développe des prestations à destination du public migrant et de la société civile. La posture adoptée par l’association repose sur la «&nbsp;facilitation&nbsp;», un soutien qui encourage à faire «&nbsp;avec&nbsp;» la personne, en la guidant pour qu’elle devienne acteur de son propre parcours.</h2>
 
-          <h3>L’accompagnement de transition :</h3>
+          <h3>L’accompagnement de transition</h3>
 
           <p>
-            l’association Relier propose des accompagnements atypiques et novateurs en s’appuyant sur la mobilisation de
-            la personne accompagnée, comme axe central de la démarche.
-            Notre approche se distingue par son caractère individualisé et flexible pour créer des espaces de rencontre
-            et d’apprentissage en dehors des cadres institutionnels.
+            L’association Relier propose un accompagnement innovant et personnalisé qui place la personne accompagnée
+            au centre de la démarche, avec pour objectif de renforcer son autonomie et son intégration. En misant
+            sur une approche flexible et individualisée, Relier offre des espaces de rencontre et d’apprentissage en
+            dehors des cadres institutionnels traditionnels.
           </p>
 
           <p>
-            L’accompagnement est orienté sur le quotidien et l’environnement de la personne favorisent l’échange
-            d’expérience, renforcent des compétences sociales et professionnelles, ainsi que l’intégration dans la
-            société, la société d’accueil.
-            En coopération avec le réseau, les actions entreprises sont coordonnées dans le but de dénouer et faire
-            évoluer la situation. La posture de facilitation fait partie intégrante de l’accompagnement pour faire avec
-            la personne et non à la&nbsp;place&nbsp;de.
+            L’accompagnement, adapté aux besoins spécifiques de chacun, se déroule directement dans l’environnement
+            quotidien de la personne, ce qui permet de
+            se concentrer sur des aspects concrets de sa vie et de favoriser le partage d’expériences, le développement
+            de compétences sociales et professionnelles, ainsi que l’intégration dans la société. La démarche inclut
+            notamment une phase de sensibilisation et de transfert de compétences, essentielle pour aider la personne à
+            identifier et utiliser les ressources de son environnement, et favoriser une responsabilisation progressive
+            et&nbsp;durable.
           </p>
           <p>
-            Le rôle de facilitateurs de transition, permets d’observer la personne dans son fonctionnement pour définir,
-            avec elle, un plan d’action favorisant son inclusion dans la cité.
-            Pour atteindre ses buts, l’association RELIER développe des partenariats dans l’environnement Genevois et
-            avec le Bureau d’Intégration Citoyenne.
+            Cet accompagnement repose sur des rencontres régulières, ajustées aux besoins de la personne qui s’intègre
+            parfaitement dans sa vie quotidienne. En s’appuyant sur les principes du "bas seuil" et de l’ «&nbsp;aller-vers&nbsp;»,
+            Relier co-construit avec les individus un cadre adapté aux spécificités de chacun.
           </p>
 
-          <h3>L’interprétariat Communautaire :</h3>
+          <h3>L’interprétariat Communautaire</h3>
 
 
           <p>
             L'interprétariat communautaire est un service essentiel qui facilite la communication entre les personnes ne
-            maîtrisant pas le français et les institutions publiques ou privées du canton de Genève.
-            Que ce soit dans les hôpitaux, les écoles, les tribunaux ou les services sociaux, nos interprètes qualifiés
-            garantissent que chaque individu puisse comprendre et être compris, assurant ainsi un accès équitable aux
-            services essentiels et la participation active à la vie locale.
+            maîtrisant pas le français et les institutions publiques ou privées du canton de Genève. Que ce soit dans
+            les hôpitaux, les écoles, les tribunaux ou les services sociaux, nos interprètes qualifié·e·s garantissent
+            que chaque individu puisse comprendre et être compris, assurant ainsi un accès équitable aux services
+            essentiels et la participation active à la vie locale.
           </p>
 
 
           <div>
             <blockquote>
-              <p>« Chacun doit avoir la possibilité d'accéder aux services publics sans être freiné par des barrières linguistiques, conformément au principe d’égalité inscrit dans la Constitution fédérale. » </p>
+              <p>«&nbsp;Chacun doit avoir la possibilité d'accéder aux services publics sans être freiné par des barrières linguistiques, conformément au principe d’égalité inscrit dans la Constitution fédérale.&nbsp;»</p>
             </blockquote>
             <p style="text-align: right">Article 8, <cite>Constitution fédérale suisse.</cite></p>
           </div>
@@ -86,8 +89,35 @@
         <h1>À propos</h1>
 
         <div class="s-page_content">
-          <div class="s-page_content" style="text-align: center">
-            <p>[à venir…]</p>
+          <div class="s-page_content">
+            <h3>Le comité</h3>
+            <p>
+              Le comité de l’association et le secrétariat général assurent le développement et le suivi des affaires
+              courantes de l’association, définissent ses objectifs et représentent ses intérêts. Il est composé de
+              professionnels issus de divers horizons et secteurs d’activités. Chaque membre s'engage à défendre les
+              valeurs de l'association RELIER. La diversité de leurs compétences et leurs expériences élargit les
+              perspectives de réflexion de l'association et lui permet de mieux répondre à sa mission d'insertion
+              sociale.
+            </p>
+
+
+            <h3>Le collège</h3>
+            <p>Le collège associatif, regroupe plusieurs dizaines de professionnel·le·s reconnue.s pour leur expérience et leur expertise, tant dans le domaine de l’accompagnement social que dans le secteur médical, entrepreneurial, associatif, culturel et syndical. Organisé comme un répertoire de professionnels, le collège associatif est une ressource précieuse, offrant expertise, conseils, connaissance des réseaux et facilitant l'opérationnalité de l’association.</p>
+
+            <h3>L’accompagnement</h3>
+            <p>Dans le cadre de son activité, l’accompagnement est organisé en binôme constitué d’un·e accompagnateur·rice social.e et d’un·e accompagnateur·rice communautaire.</p>
+
+            <h3>L’accompagnateur·rice social·e</h3>
+            <p>L’accompagnateur·rice social·e a pour mission de renforcer le travail de réseau autour de la personne accompagnée, en coordonnant les différents acteur·rice·s impliqué·e·s dans la situation. Iel collabore tout autant avec les travailleur·euse·s sociaux·ales qu’avec le corps médical pour évaluer les besoins et favoriser l’adhésion de la personne. L’objectif est de faciliter la compréhension des démarches administratives et de renforcer l’accès aux soins dans le pays d’accueil.</p>
+            <p>L’accompagnement met également l’accent sur l’insertion sociale et communautaire, offrant des repères dans l’environnement d’accueil et élargissant les possibilités d’inclusion. Ces dernières pouvant s’exprimer au travers de collaborations inscrites dans le tissu associatif et/ou économique.</p>
+            <p>En valorisant les compétences individuelles, professionnelles et sociales de la personne, ainsi que son savoir-faire, Relier soutient la mise en place d’activités favorisant l’apprentissage de la langue et le processus d’acculturation. Nous partons du principe que les compétences acquises au cours de l’expérience de vie constituent des leviers essentiels pour leur inclusion sociale et leur stabilité.</p>
+
+            <h3>L’accompagnateur·rice communautaire</h3>
+            <p>Relier met un point d’honneur à intégrer un·e accompagnateur·rice communautaire de référence dans chaque situation, pour renforcer le lien et la</p>
+            <p>confiance. La démarche s’attelle à transposer et traduire les besoins spécifiques dans l'optique d’une meilleure intégration au sein de la société d’accueil des personnes accompagnées.</p>
+            <p>Présent tout au long de processus, l’accompagnateur·rice communautaire est sollicité·e à chaque phase du dispositif. Ne se limitant pas au seul rôle d’interprète, iel permet un apprentissage mutuel des codes culturels auprès des différent·e·s acteur·rice·s favorisant ainsi la construction d'une approche collective cohérente et concertée.</p>
+
+
           </div>
         </div>
 
